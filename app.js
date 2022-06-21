d43c78509acc6b4cab90a38632bfc05c1b4397f9
Diar J R <diar.student@gmail.com>
2022-06-21 09:17:43 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 var multer = require("multer");
 var upload = multer();
 const port = process.env.PORT || 3000;
